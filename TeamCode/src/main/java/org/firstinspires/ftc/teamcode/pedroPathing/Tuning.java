462ced591da56e2390b5fac93bf5080371a00f43
kyled <k.pilkington@harrissutton.org.uk>
2026-01-21 15:42:46 +0000

    Changed forward velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Tuning.java
@@ -528,7 +528,7 @@ class LateralVelocityTuner extends OpMode {
  */
 class ForwardZeroPowerAccelerationTuner extends OpMode {
     private final ArrayList<Double> accelerations = new ArrayList<>();
-    public static double VELOCITY = 20;
+    public static double VELOCITY = 30;
 
     private double previousVelocity;
     private long previousTimeNano;
