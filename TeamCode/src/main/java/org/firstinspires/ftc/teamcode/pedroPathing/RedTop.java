7a812ae50a35adc7bfa2755afb1b9e91df4debe5
kyled <k.pilkington@harrissutton.org.uk>
2026-03-25 19:52:12 +0000

    Mirrored points to Redtop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/RedTop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/RedTop.java
@@ -24,13 +24,13 @@ public class RedTop extends OpMode {
     private Servo rightBarrier;
     public Timer pathTimer, opmodeTimer, actionTimer;
     private int pathState;
-    private final Pose startPose = new Pose(34.543, 134.752, Math.toRadians(270)); // Start Pose of our robot.
-    private final Pose scorePose = new Pose(63.613, 83.396, Math.toRadians(140)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
-    private final Pose pickup1Pose = new Pose(17.4, 85.029, Math.toRadians(180)); // Highest (First Set) of Artifacts from the Spike Mark.
-    private final Pose control1=new Pose(68.465,57.191);
-    private final Pose control2=new Pose(75.685,31.519);
-    private final Pose pickup2Pose = new Pose(15.4, 62.561, Math.toRadians(180)); // Middle (Second Set) of Artifacts from the Spike Mark.
-    private final Pose pickup3Pose = new Pose(15.4, 35.304, Math.toRadians(180)); // Lowest (Third Set) of Artifacts from the Spike Mark.
+    private final Pose startPose = new Pose(109.457, 134.752, Math.toRadians(270)); // Start Pose of our robot.
+    private final Pose scorePose = new Pose(63.613, 83.396, Math.toRadians(50)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
+    private final Pose pickup1Pose = new Pose(126.6, 85.029, Math.toRadians(0)); // Highest (First Set) of Artifacts from the Spike Mark.
+    private final Pose control1=new Pose(75.535,57.191);
+    private final Pose control2=new Pose(68.315,31.519);
+    private final Pose pickup2Pose = new Pose(128.6, 62.561, Math.toRadians(0)); // Middle (Second Set) of Artifacts from the Spike Mark.
+    private final Pose pickup3Pose = new Pose(128.6, 35.304, Math.toRadians(0)); // Lowest (Third Set) of Artifacts from the Spike Mark.
 
     private Path scorePreload;
     private PathChain grabPickup1, scorePickup1,grabPickup2, scorePickup2, grabPickup3, scorePickup3;
